1b5f4d1fb1182a47afe94fbbd850a2952030cf01
phaeena <phaeena@users.noreply.github.com>
2020-12-27 01:28:54 +0100

    Update udemyScraper.py
    Google's updated search results page, so do I

--- udemyScraper.py
+++ udemyScraper.py
@@ -6,7 +6,7 @@ init(autoreset=True)
 
 query = "inurl:udemy.com/course/ \"inurl:couponCode"
 # tbs variable sets the time range (e.g.: d = day; m = month)
-tbs = "d" 
+tbs = "m" 
 URL = f"https://google.com/search?q={query}&tbs=qdr:{tbs}"
 
 
@@ -22,7 +22,7 @@ if resp.status_code == 200:
     soup = BeautifulSoup(resp.content, "html.parser")
 
 link, title, coupon = [], [], []
-for g in soup.find_all('div', class_='r'):
+for g in soup.find_all('div', class_='g'):
     anchors = g.find_all('a')
     if anchors:
         title.append(g.find('h3').text)
@@ -36,11 +36,14 @@ for i in range(len(results)):
     resp = requests.get(results[i]["Link"], headers=headers)
 
     if resp.status_code == 200:
-        course_code = BeautifulSoup(resp.content, "html.parser").body['data-clp-course-id']
+        try:
+            course_code = BeautifulSoup(resp.content, "html.parser").body.attrs['data-clp-course-id']
+        except KeyError:
+            pass
         
         CodeCheck_URL = f"https://www.udemy.com/api-2.0/course-landing-components/{course_code}/me/?components=buy_button,purchase,redeem_coupon,discount_expiration,gift_this_course&discountCode="+results[i]["Coupon"]
 
-        resp = requests.request("GET",CodeCheck_URL, headers=headers).json()
+        resp = requests.request("GET", CodeCheck_URL, headers=headers).json()
         
         # check if the the price is free and the code isn't yet expired
         if (resp)["purchase"]["data"]["pricing_result"]["price"]["amount"] == 0:
@@ -50,4 +53,3 @@ for i in range(len(results)):
             print(f"\t{Fore.GREEN}Coupon: {Style.RESET_ALL}"+results[i]["Coupon"]+ 
                 f"\t\t{Fore.GREEN}Coupon Remaining: {Style.RESET_ALL}"+str((resp)["purchase"]["data"]["pricing_result"]["campaign"]["uses_remaining"])
                 +str(f"\t\t{Fore.GREEN}End Time: {Style.RESET_ALL}"+str((resp)["purchase"]["data"]["pricing_result"]["campaign"]["end_time"])[:-9])) 
-      
